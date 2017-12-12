c2dfb9552b50455bbdf9fd45d1bbe0ec558e127c
H F <jonis88@gmail.com>
2017-12-12 13:04:26 +0100

    Update validation

--- Garage2/Garage2/Models/ParkedVehicle.cs
+++ Garage2/Garage2/Models/ParkedVehicle.cs
@@ -11,19 +11,25 @@ namespace Garage2.Models
     {
         public int Id { get; set; }
 
+        [Required]
         [Display(Name = "Type of vehicle")]
         public Type TypeOf { get; set; }
 
+        [Required]
+        [RegularExpression(@"^[A-Za-z]{3}(\d{3})$", ErrorMessage = "Wrong format")]
         [Display(Name = "Registration number")]
         public string Registration { get; set; }
 
         [Display(Name = "Color")]
+        [RegularExpression(@"^[a-zA-Z'' ']+$", ErrorMessage = "Special character & Numbers should not be entered")]
         public string Color { get; set; }
 
         [Display(Name = "Model")]
+        [RegularExpression(@"^[a-zA-Z'' ']+$", ErrorMessage = "Special character & Numbers should not be entered")]
         public string Model { get; set; }
 
         [Display(Name = "Number of wheels")]
+        [Range(0, 1000)]
         public string NumberOfWheels { get; set; }
 
         [Display(Name = "Time of arrival")]
